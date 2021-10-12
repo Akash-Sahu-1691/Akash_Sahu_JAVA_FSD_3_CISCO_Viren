761675c53d02d7cdb0f1c5ce309f8bd07360e705
AKASH SAHU <67486242+Deadshot-OO7@users.noreply.github.com>
2021-10-12 17:14:29 +0530

    Delete -Akash_Sahu_JAVA_FSD_3_CISCO_Viren-master/-Akash_Sahu_JAVA_FSD_3_CISCO_Viren-master/Phase3project directory

--- -Akash_Sahu_JAVA_FSD_3_CISCO_Viren-master/-Akash_Sahu_JAVA_FSD_3_CISCO_Viren-master/Phase3project/src/main/java/com/example/repository/UserRepo.java
+++
@@ -1,9 +0,0 @@
-package com.example.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.example.model.User;
-
-public interface UserRepo extends CrudRepository<User, Integer>{
-
-}
